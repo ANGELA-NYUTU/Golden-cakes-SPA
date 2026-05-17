d9ccce65e461496468e8e5d36400088b713ecade
ANGELA-NYUTU <angie.nimo254@gmail.com>
2026-05-17 08:45:55 +0300

    initial commit

---
+++ golden-cakes/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-4xl font-bold text-[#0033a0]">
+        Golden Crumbs Cake Shop
+      </h1>
+
+      <p className="mt-4 text-lg">
+        Welcome to the cake shop admin portal.
+      </p>
+    </div>
+  )
+}
+
+export default Home
